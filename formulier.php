a13f61e4aa86920555b921cae95bf558b2907352
oguzhanarslan <oguzhan28@hotmail.nl>
2015-12-11 09:49:44 +0100

    no message

--- formulier.php
+++ formulier.php
@@ -6,6 +6,7 @@
 .error {color: #FF0000;}
 </style>
 <link href="style.css" rel="stylesheet"> 
+  <script src='script.js'></script>
 </head>
 <body>
 <a class="home" href="index.php"><img src="logo.png" style height="150px" width="100px"></a>
@@ -47,21 +48,25 @@ $summary = $_POST["summary"];
 <h1>Formulier</h1>
 <p class="error"><span class="error">* required field.</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-<label for="USERNAME">Username: </label><input type="text" name="author" value="">
+<label for="USERNAME">Username: </label><input type="text" name="author" id="author" value="">
 <span class="error">* <?php echo $authorErr;?></span>
 <br><br>
-<label for="TITLE">Title: </label><input type="text" name="title" value="">
+<label for="TITLE">Title: </label><input type="text" id="title" name="title" value="">
 <span class="error">* <?php echo $titleErr;?></span>
 <br><br>
-<label for="URL">URL: </label><input type="text" name="url" value="">
+<label for="URL">URL: </label><input type="text" id="url" name="url" value="">
 <span class="error">* <?php echo $urlErr;?></span>
 <br><br>
-<label for="SUMMARY">Summary: </label><textarea name="summary" rows="5" cols="40"></textarea>
+<label for="SUMMARY">Summary: </label><textarea id="summary" name="summary" rows="5" cols="40"></textarea>
 <span class="error">* <?php echo $summaryErr;?></span>
 <br><br>
-<input type="submit" name="submit" value="Submit" >
+<input type="submit" id="submit" name="submit" value="Submit" >
 </form>
 </div>
+<form name="myForm" action="demo_form.asp" onsubmit="return validateForm()" method="post">
+Name: <input type="text" name="fname">
+<input type="submit" value="Submit">
+</form>
 
 <?php
 //Create connection
